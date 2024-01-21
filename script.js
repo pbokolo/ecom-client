cac4941c111881bfd8598223108f5c1be4943c79
pbokolo <dev.pbokolo@gmail.com>
2024-01-21 06:57:56 +0200

    Programmatically opening and closing the dialog

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 "use strict";
 const addBtn = document.querySelector("#addBtn");
+const dialog = document.querySelector("#dialog");
+const overlay = dialog.querySelector("#overlay");
 
-addBtn.addEventListener("click", () => console.log("Should add"));
+addBtn.addEventListener("click", () => dialog.showModal());
+overlay.addEventListener("click", (e) => {
+  if (e.target.id !== "overlay") return;
+  dialog.close();
+});
